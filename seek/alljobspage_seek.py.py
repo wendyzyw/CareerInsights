cf91543f21efcf669ca04ede726099c1e4d88755
Hanqi Liu <953845518@qq.com>
2017-12-22 00:39:35 +1100

    Update alljobspage_seek.py.py

--- seek/alljobspage_seek.py.py
+++ seek/alljobspage_seek.py.py
@@ -33,41 +33,7 @@ for i in range(0, len(job_list)-1):
 	temp_str = job_list[i].replace('{','').replace('}','').replace('"','')
 	temp_list = re.split('[,:]', temp_str)
 	info_list.append(temp_list)
-
+print info_list
 with open("output.csv", "wb") as f:
 	writer = csv.writer(f)
 	writer.writerows(info_list)
-'''
-with open("output.csv", "wb") as f:
-	writer = csv.writer(f)
-	writer.writerows(row_list)
-'''
-'''
-for word in temp_str:
-		print word
-		temp.append(word)
-	row_list.append(temp)
-'''
-'''
-for char in job_list[i]:
-		if char in '}"{':
-			temp_str = job_list[i].replace(char, '')
-'''
-'''
-word_list[][] = ""
-for i in range(0, len(job_list)-1):
-	word_list[i].append(job_list[i].split(","))
-
-replace_punctuation = string.maketrans(string.punctuation,' '*len(string.punctuation))
-	temp_str = job_list[i].translate(replace_punctuation)
-	temp = re.split('\s+',temp_str)
-	row_list.append(temp)
-'''
-'''
-
-'''
-'''
-with open("output.csv", "wb") as f:
-	writer = csv.writer(f)
-	writer.writerows(job_list)
-'''
